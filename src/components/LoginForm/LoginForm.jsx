968aafa2d8790d20d831311865a999fe9729d3bc
MateuszBachor <mateuszbachor3@gmail.com>
2022-08-23 22:36:09 +0200

    asdadvda

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import AddContactForm from '../AddContactForm/AddContactForm';
 import FindContact from 'components/FindContact/FindContact';
 import ContactList from 'components/ContactList/ContactList';
-// import styles from './LoginForm.module.css';
+import styles from './LoginForm.module.css';
 
 export class LoginForm extends React.Component {
   state = {
@@ -51,7 +51,7 @@ export class LoginForm extends React.Component {
   render() {
     const { filter, contacts } = this.state;
     return (
-      <div>
+      <div className={styles.whatever}>
         <AddContactForm SubmitForm={this.handleSubmit} />
         <FindContact findContact={this.find} />
         <ContactList
